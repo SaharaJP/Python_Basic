cfedee720eb6bae865799427b58717a0f3c79efe
Андрей <icreem714@gmail.com>
2022-09-14 12:51:39 +0300

    14.3 Написал две функции для решения решения задачи на разницу суммы чисел и кол-ва цифр в числе

--- Module14/03_sum_and_dif/main.py
+++ Module14/03_sum_and_dif/main.py
@@ -1,1 +1,20 @@
-# TODO здесь писать код
+def summ(num):
+    summ = 0
+    while num > 0:
+        summ += num % 10
+        num //= 10
+    print('\nСумма чисел:', summ)
+    return summ
+
+def count_num(num):
+    count = 0
+    for cifer_summ in num:
+        count += 1
+    print('Количество цифр в числе:', count)
+    return count
+
+
+num = int(input('Введите число: '))
+difference = summ(num) - count_num(str(num))
+
+print('Разность суммы и количество цифр:', difference)
