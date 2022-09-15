40812b4d87fa50d8041c9779349c440809d83d7b
Андрей <icreem714@gmail.com>
2022-09-15 13:03:24 +0300

    14.4 Задача выполнена

--- Module14/04_reverse_num/main.py
+++ Module14/04_reverse_num/main.py
@@ -1,2 +1,37 @@
 
+def point_counting(num): #Записывает число знаков после запятой
+    count = 0
+    while num - round(num) > 0:
+        num *= 10
+        count += 1
+    return count
 
+
+def backward_counting(num): #Записывает число наоборот
+    backward = ''
+    for back_num in str(num):
+        backward = back_num + backward
+    return backward
+
+
+def backward_num(num):
+    before_point = round(num)
+    after_point = num - before_point
+
+    back_before_point = float(backward_counting(before_point))
+    back_after_point = float(backward_counting(round(after_point, 10)))
+    divider = point_counting(num)
+
+    backward_num = back_before_point + back_after_point / (10 ** divider)
+    return backward_num
+
+
+first_num = float(input('Введите первое число: '))
+second_num = float(input('Введите второе число: '))
+
+first_backward_num = backward_num(first_num)
+second_backward_num = backward_num(second_num)
+
+print('\nПервое число наоборот:', first_backward_num)
+print('Второе число наоборот:', second_backward_num)
+print('Сумма:', first_backward_num + second_backward_num)
