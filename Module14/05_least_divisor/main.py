0a648d981bb90b3ff0ac96b15875bf19598c955a
Андрей <icreem714@gmail.com>
2022-09-17 18:06:22 +0300

    14.5 Задача выполнена

--- Module14/05_least_divisor/main.py
+++ Module14/05_least_divisor/main.py
@@ -1,1 +1,11 @@
-# TODO здесь писать код
+
+def min_divider():
+    for divider in range(2, number + 1):
+        if number % divider == 0:
+            print('Наименьший общий делитель:', divider)
+            break
+
+
+number = int(input('Введите число: '))
+
+min_divider()
