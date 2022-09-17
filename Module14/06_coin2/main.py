8adc6a450bb84ff411c9e03421d9b98e301d5854
Андрей <icreem714@gmail.com>
2022-09-17 18:26:13 +0300

    14.6 Задача выполнена

--- Module14/06_coin2/main.py
+++ Module14/06_coin2/main.py
@@ -1,1 +1,14 @@
-# TODO здесь писать код
+
+def coin_area():
+    if abs(x) - r <= 0 and abs(y) - r <= 0:
+        print('\nМонетка где-то рядом')
+    else:
+        print('\nМонетки в области нет')
+
+
+print('Введите координаты монеты:')
+x = float(input('X: '))
+y = float(input('Y: '))
+r = int(input('Введите радиус: '))
+
+coin_area()
