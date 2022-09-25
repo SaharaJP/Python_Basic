43b1b8c70c55bd334e241fe7c4302c0dbd070626
Андрей <icreem714@gmail.com>
2022-09-25 12:14:26 +0300

    Задача выполнена

--- Module16/02_line/main.py
+++ Module16/02_line/main.py
@@ -1,1 +1,18 @@
-# TODO здесь писать код
+def sort_all_class_list(all_class_list): #Сортирует список всех учеников по возрастанию
+    for i_student in range(len(all_class_list)):
+        for i_all_students in range(i_student, len(all_class_list)):
+            if all_class_list[i_all_students] < all_class_list[i_student]:
+                all_class_list[i_all_students], all_class_list[i_student] = all_class_list[i_student], all_class_list[i_all_students]
+    return all_class_list
+
+
+first_class_list = list(range(160, 177, 2))
+second_class_list = list(range(162, 181, 3))
+all_class_list = []
+
+all_class_list.extend(first_class_list)
+all_class_list.extend(second_class_list)
+
+#all_class_list.sort()
+
+print('Отсортированный список учеников:', sort_all_class_list(all_class_list))
