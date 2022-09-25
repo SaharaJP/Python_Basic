4699c66a2032c18216b161f47bc9cc5509cb9cec
Андрей <icreem714@gmail.com>
2022-09-26 01:26:44 +0300

    Задача выполнена

--- Module16/08_rhyme_cnt/main.py
+++ Module16/08_rhyme_cnt/main.py
@@ -1,23 +1,19 @@
-people_count = int(input('Кол-во человек: '))
-people_count = list(range(1, people_count + 1))
+people = int(input('Кол-во человек: '))
 rhyme = int(input('Какое число в считалке? '))
-start = 1
-count = 0
+people_list = list(range(1, people+ 1))
+out = 0
 
-print('Значит, выбывает каждый ' + str(rhyme) + '-й человек')
-print('\nТекущий круг людей:', people_count)
-print('Начало счета с номера', start)
+print('Значит, выбывает каждый ' + str(rhyme) + '-й человек.')
 
-# while count < rhyme:
-#     for num in people_count:
-#         count += 1
-#         if count == rhyme:
-#             people_count.remove(num)
+while len(people_list) > 1:
+   print('\nТекущий круг людей', people_list)
 
-while len(people_count) > 1:
+   start = out % len(people_list)
+   out = (start + rhyme - 1) % len(people_list)
 
-    if rhyme > len(people_count):
-        people_count.remove(abs(rhyme - len(people_count)))
-    else:
-        people_count.remove(rhyme)
-    print(people_count)
+   print('Начало счёта с номера', people_list[start])
+   print('Выбывает человек под номером', people_list[out])
+
+   people_list.remove(people_list[out])
+
+print('Остался человек под номером', people_list[0])
