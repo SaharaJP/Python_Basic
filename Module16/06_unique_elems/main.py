4b15b7f1d9d4f172cc89004eaf76dbcf3110432f
Андрей <icreem714@gmail.com>
2022-09-25 13:48:42 +0300

    Задача выполнена

--- Module16/06_unique_elems/main.py
+++ Module16/06_unique_elems/main.py
@@ -1,1 +1,23 @@
-# TODO здесь писать код
+first_list = []
+second_list = []
+
+for i in range(3):
+    print('Введите ' + str(i + 1) + '-е число для первого списка:', end = ' ')
+    num = int(input())
+    first_list.append(num)
+
+for i in range(7):
+    print('Введите ' + str(i + 1) + '-е число для второго списка:', end=' ')
+    num = int(input())
+    second_list.append(num)
+
+print('\nПервый список:', first_list)
+print('Второй список:', second_list)
+
+first_list.extend(second_list)
+
+for i in first_list:
+    while first_list.count(i) > 1:
+        first_list.remove(i)
+
+print('\nНовый список с уникальными элементами:', first_list)
