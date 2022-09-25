66457ad6ea6a70d14c44f1dea3c1dfe2d17f4cb9
Андрей <icreem714@gmail.com>
2022-09-26 00:15:10 +0300

    Задача выполнена

--- Module16/10_simmetrical_seq/main.py
+++ Module16/10_simmetrical_seq/main.py
@@ -1,1 +1,19 @@
-# TODO здесь писать код
+num_count = int(input('Кол-во чисел: '))
+subsequence = []
+need_to_add = []
+
+for _ in range(num_count):
+    num = int(input('Число: '))
+    subsequence.append(num)
+
+if subsequence[-1] == subsequence[-2] and subsequence[-1] != subsequence[-3]:
+    for i in range(len(subsequence) - 3, -1, -1):
+        need_to_add.append(subsequence[i])
+
+elif subsequence[-1] != subsequence[-2]:
+    for i in range(len(subsequence) - 2, -1, -1):
+        need_to_add.append(subsequence[i])
+
+print('\nПоследовательность:', subsequence)
+print('Нужно приписать чисел:', len(need_to_add))
+print('Сами числа:', need_to_add)
