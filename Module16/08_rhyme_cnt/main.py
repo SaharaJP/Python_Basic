9edd9b6fbc5c5aa97e1820b46cefe234d7ad3347
Андрей <icreem714@gmail.com>
2022-09-25 23:56:45 +0300

    Задача выполнена

--- Module16/08_rhyme_cnt/main.py
+++ Module16/08_rhyme_cnt/main.py
@@ -1,1 +1,23 @@
-# TODO здесь писать код
+people_count = int(input('Кол-во человек: '))
+people_count = list(range(1, people_count + 1))
+rhyme = int(input('Какое число в считалке? '))
+start = 1
+count = 0
+
+print('Значит, выбывает каждый ' + str(rhyme) + '-й человек')
+print('\nТекущий круг людей:', people_count)
+print('Начало счета с номера', start)
+
+# while count < rhyme:
+#     for num in people_count:
+#         count += 1
+#         if count == rhyme:
+#             people_count.remove(num)
+
+while len(people_count) > 1:
+
+    if rhyme > len(people_count):
+        people_count.remove(abs(rhyme - len(people_count)))
+    else:
+        people_count.remove(rhyme)
+    print(people_count)
--- Module16/09_friends/main.py
+++ Module16/09_friends/main.py
@@ -1,1 +1,21 @@
-# TODO здесь писать код
+friends = int(input('Кол-во друзей: '))
+credit = int(input('Долговых расписок: '))
+balances = []
+
+for i in range(friends):
+    balances.append([i + 1, 0])
+
+for i in range(credit):
+    print('\n' + str(i + 1) + '-я расписка')
+    friend_to = int(input('Кому: '))
+    friend_from = int(input('От кого: '))
+    how_much = int(input('Сколько: '))
+
+    balances[friend_to - 1][1] -= how_much
+    balances[friend_from - 1][1] += how_much
+
+else:
+    print('\nБаланс друзей:')
+    for i in range(friends):
+        print(balances[i][0], end = ' : ')
+        print(balances[i][1])
