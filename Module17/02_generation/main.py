fc65604d0e0a5d29b6f2b6eb9630867841f89426
Андрей <icreem714@gmail.com>
2022-09-28 12:03:18 +0300

    Задача выполнена

--- Module17/02_generation/main.py
+++ Module17/02_generation/main.py
@@ -1,1 +1,5 @@
-# TODO здесь писать код
+N = int(input('Введите длину списка: '))
+result = list(range(N))
+result = [1 if i % 2 == 0 else i % 5
+          for i in result]
+print('Резултат:', result)
