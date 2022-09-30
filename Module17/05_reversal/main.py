ab1a6826b3059833f11478ca88aefa162f432aaa
Андрей <icreem714@gmail.com>
2022-09-30 22:55:18 +0300

    Задача выполнена

--- Module17/05_reversal/main.py
+++ Module17/05_reversal/main.py
@@ -1,1 +1,14 @@
-# TODO здесь писать код
+text = input('Введите строку: ')
+rev_text = text[::-1]
+
+left = text.index('h')
+right = rev_text.index('h')
+
+if rev_text.index('h') == 0:
+    i = 1
+else:
+    i = -1
+
+rev_text = rev_text[- right + i : left - 1]
+
+print('Развернутая последовательность между первым и последним h:', rev_text)
