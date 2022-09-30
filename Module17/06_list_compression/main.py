75aace72f5551e9cffb8cdc32cdef926724b2903
Андрей <icreem714@gmail.com>
2022-09-30 23:38:58 +0300

    Задача выполнена

--- Module17/06_list_compression/main.py
+++ Module17/06_list_compression/main.py
@@ -1,1 +1,14 @@
-# TODO здесь писать код
+import random
+
+num_count = int(input('Количество чисел в списке: '))
+num_list = [random.randint(0, 2) for _ in range(num_count)]
+
+count = 0
+for i in range(num_count):
+    if num_list[i] > 0:
+        num_list[i], num_list[count] = num_list[count], num_list[i]
+        count += 1
+
+num_list = num_list[:num_list.index(0)]
+
+print('Список после сжатия:', num_list)
