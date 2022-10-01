df04fe9c8858205e9cd2aa099f95f7d9f58c87b4
Андрей <icreem714@gmail.com>
2022-10-01 21:03:18 +0300

    Задача выполнена

--- Module17/10_caesar_cipher/main.py
+++ Module17/10_caesar_cipher/main.py
@@ -1,1 +1,14 @@
-# TODO здесь писать код
+alphabet = 'абвгдеёжзийклмнопрстуфхцчшщъыьэюя'
+
+text = input('Введите сообщение: ')
+shift = int(input('Введите сдвиг: '))
+
+caesar_text = text[:]
+caesar_text = [(alphabet[(alphabet.index(sym) + shift) % 33]
+                if sym != ' ' else ' ') for sym in caesar_text]
+
+result = ''
+for sym in caesar_text:
+    result += sym
+
+print('Зашифрованное сообщение:', result)
