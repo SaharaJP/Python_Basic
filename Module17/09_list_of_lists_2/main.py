c4eda7f6aa59c5d5ddf6efc16fcc844168fe3908
Андрей <icreem714@gmail.com>
2022-10-01 20:01:43 +0300

    Задача выполнена

--- Module17/09_list_of_lists_2/main.py
+++ Module17/09_list_of_lists_2/main.py
@@ -1,4 +1,8 @@
 nice_list = [[[1, 2, 3], [4, 5, 6], [7, 8, 9]],
              [[10, 11, 12], [13, 14, 15], [16, 17, 18]]]
 
-# TODO здесь писать код
+nice_list = [s_el for b_el in nice_list
+             for m_el in b_el
+             for s_el in m_el]
+
+print('Ответ:', nice_list)
