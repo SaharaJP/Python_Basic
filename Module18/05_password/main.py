3765e8ff0ee491b92f5e806d38a0676c844cfadc
Андрей <icreem714@gmail.com>
2022-10-04 00:47:41 +0300

    Задача выполнена

--- Module18/05_password/main.py
+++ Module18/05_password/main.py
@@ -1,1 +1,16 @@
-# TODO здесь писать код
+while True:
+    password = list(input('Введите пароль: '))
+    upper_letters_count = len(
+                            [letter for letter in password
+                            if letter.isupper()]
+                              )
+    cyfer_count = len(
+                    [num for num in password
+                    if num.isdigit()]
+                      )
+
+    if upper_letters_count >= 1 and cyfer_count >= 3:
+        print('Это надежный пароль!')
+        break
+    else:
+        print('Пароль ненадеждный. Попробуйте еще раз.')
