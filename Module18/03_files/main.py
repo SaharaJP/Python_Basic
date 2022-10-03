66b5691b6c4f1262a3f5f20f11bc233a74aa4ccf
Андрей <icreem714@gmail.com>
2022-10-04 00:31:15 +0300

    Задача выполнена

--- Module18/03_files/main.py
+++ Module18/03_files/main.py
@@ -1,1 +1,17 @@
-# TODO здесь писать код
+not_allowed_sym = '@№$%^&\*()'
+allowed_extencions = ['.txt', '.docx']
+
+file_name = input('Введите название файла: ')
+
+for sym in not_allowed_sym:
+    if file_name.startswith(sym):
+        print('Ошибка: название начинается на один из специальных символов')
+        break
+
+for extencions in allowed_extencions:
+    if not file_name.endswith(extencions):
+        print('Ошибка: неверное расширение файла. Ожидалось ".txt" или ".docx"')
+        break
+    else:
+        print('Файл назван верно')
+        break
