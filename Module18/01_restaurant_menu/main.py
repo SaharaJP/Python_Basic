524afbaf673062b6212fad6ab830f0e2b8a946dd
Андрей <icreem714@gmail.com>
2022-10-03 23:20:22 +0300

    Задача выполнена

--- Module18/01_restaurant_menu/main.py
+++ Module18/01_restaurant_menu/main.py
@@ -1,1 +1,5 @@
-# TODO здесь писать код
+available_menu = input('Доступное меню (осуществляйте ввод через ";"): ').split(';')
+available_menu_str = ', '.join(available_menu)
+
+# print('На данный момент в меню есть:', ', '.join(available_menu))
+print('На данный момент в меню есть:', available_menu_str)
