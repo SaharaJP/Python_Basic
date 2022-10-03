f75c25fbbc1dc7fdb22cd77f3620c22bca212705
Андрей <icreem714@gmail.com>
2022-10-04 00:00:48 +0300

    Задача выполнена

--- Module18/02_longest_word/main.py
+++ Module18/02_longest_word/main.py
@@ -1,1 +1,9 @@
-# TODO здесь писать код
+text = input('Введите строку: ').split()
+most_len_word = text[0]
+
+for word in text:
+    if len(word) > len(most_len_word):
+        most_len_word = word
+
+print('Самое длинное слово:', most_len_word)
+print('Длина этого слова:', len(most_len_word))
