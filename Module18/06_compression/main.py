0a7408aa820b0d5369be9a4866ff2b037606ebd8
Андрей <icreem714@gmail.com>
2022-10-04 11:36:37 +0300

    Задача выполнена

--- Module18/06_compression/main.py
+++ Module18/06_compression/main.py
@@ -1,1 +1,12 @@
-# TODO здесь писать код
+text = input('Введите строку: ')
+encoded_text = []
+count = 1
+
+for sym in range(len(text)):
+    if text[sym] == text[sym + 1 : sym + 2]:
+        count += 1
+        continue
+    encoded_text.append(text[sym] + str(count))
+    count = 1
+
+print('Закодированная строка: {}'.format(''.join(encoded_text)))
