2635956cdc6be18a6bbb460e55dbeb2ff17a25ee
Андрей <icreem714@gmail.com>
2022-10-06 00:11:11 +0300

    Задача выполнена

--- Module18/09_message/main.py
+++ Module18/09_message/main.py
@@ -1,1 +1,19 @@
-# TODO здесь писать код
+message = input('Сообщение: ').split()
+result = ''
+
+new_message = [list(word[::-1])
+               for word in message
+               ]
+
+for r_word in new_message:
+    for sym in r_word:
+
+        if not sym.isalpha():
+            r_word.insert(len(r_word[r_word.index(sym):]) , sym)
+            r_word.remove(r_word[r_word.index(sym)])
+            break
+
+    r_word = ''.join(r_word)
+    result += r_word + ' '
+
+print(result)
