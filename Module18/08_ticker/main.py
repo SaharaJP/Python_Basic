e7701f2687ad0c03911d557018bacf65d06e6b53
Андрей <icreem714@gmail.com>
2022-10-05 12:35:41 +0300

    Задача выполнена

--- Module18/08_ticker/main.py
+++ Module18/08_ticker/main.py
@@ -1,1 +1,10 @@
-# TODO здесь писать код
+f_string = input('Первая строка: ')
+s_string = input('Вторая строка: ')
+count = 0
+
+for i in range(1, len(f_string) + 1):
+    if s_string.endswith(f_string[:i]) and s_string.startswith(f_string[i:]):
+        print('Первая строка получается из второй со сдвигом', i)
+        break
+else:
+    print('Первую строку нельзя получить из второй с помощью циклического сдвига')
