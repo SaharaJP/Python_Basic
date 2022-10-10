01bb11867598e7f19c4df8caa56478942b51e11d
Андрей <icreem714@gmail.com>
2022-10-10 15:45:18 +0300

    Задача выполнена

--- Module19/06_synonym_dict/main.py
+++ Module19/06_synonym_dict/main.py
@@ -1,1 +1,24 @@
-# TODO здесь писать код
+pair_count = int(input('Введите количество пар слов: '))
+pairs_dict = dict()
+
+for num in range(pair_count):
+    pair = input('{0} пара: '.format(num + 1)).split(' - ')
+    pairs_dict[pair[0]] = pair[1]
+
+while True:
+    word = input('\nВведите слово (для выхода введите "end"): ').lower()
+
+    if word == 'end':
+        break
+
+    for sin in pairs_dict:
+        if pairs_dict[sin].lower() == word:
+            print('Синоним:', sin)
+            break
+
+        elif sin.lower() == word:
+            print('Синоним:', pairs_dict[sin])
+            break
+    else:
+        print('Такого слова в словаре нет')
+
