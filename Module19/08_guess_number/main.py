1752768a437d20d37e7a2ee515d8986b8f8512ba
Андрей <icreem714@gmail.com>
2022-10-11 22:37:02 +0300

    Задача выполнена

--- Module19/08_guess_number/main.py
+++ Module19/08_guess_number/main.py
@@ -1,1 +1,23 @@
-# TODO здесь писать код
+import random
+
+N = int(input('Введите максимальное число: '))
+rand_num = random.randint(1, N)
+
+while True:
+    nums = input('\nНужное число есть среди вот этих чисел: ').split()
+
+    if nums[0] == 'Помогите!':
+        print('Артем мог загадать следующие числа:', in_set.difference(not_in_set))
+        break
+
+    num_set = set(
+        {int(cyfer)
+         for cyfer in nums}
+    )
+
+    if rand_num in num_set:
+        print('Ответ Артема: Да')
+        in_set = num_set
+    else:
+        print('Ответ Артема: Нет')
+        not_in_set = num_set
