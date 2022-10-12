0e6662515e78ff7c36dcb88dca3960b2f0dea4e0
Андрей <icreem714@gmail.com>
2022-10-12 12:06:54 +0300

    Задача выполнена

--- Module19/10_palindrome_again/main.py
+++ Module19/10_palindrome_again/main.py
@@ -1,1 +1,15 @@
-# TODO здесь писать код
+text = input('Введите строку: ')
+sym_dict = dict()
+
+for sym in text:
+    sym_dict[sym] = sym_dict.get(sym, 0) + 1
+
+odd_count = 0
+for value in sym_dict.values():
+    if value % 2 != 0:
+        odd_count += 1
+
+if odd_count <= 1:
+    print('Можно сделать палиндром')
+else:
+    print('Нельзя сделать палиндром')
