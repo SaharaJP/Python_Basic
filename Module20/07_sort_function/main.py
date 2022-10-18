268dd64c8bb52c4c223290ea23ceb1436e564b50
Андрей <icreem714@gmail.com>
2022-10-18 12:38:34 +0300

    Задача выполнена

--- Module20/07_sort_function/main.py
+++ Module20/07_sort_function/main.py
@@ -1,1 +1,8 @@
-# TODO здесь писать код
+def tpl_sort(exercise):
+    for sym in exercise:
+        if str(sym).isalpha() or not str(abs(sym)).isdigit():
+            return exercise
+    else:
+        return tuple(sorted(exercise))
+
+# print(tpl_sort((6, 3, -1, 8, 4, 10, -5)))
