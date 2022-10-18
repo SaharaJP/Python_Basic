287d0820902c2781ca3d0c562f8bc8b4ebb4828d
Андрей <icreem714@gmail.com>
2022-10-18 12:40:28 +0300

    Задача выполнена

--- Module20/06_pairs/main.py
+++ Module20/06_pairs/main.py
@@ -1,1 +1,16 @@
-# TODO здесь писать код
+import random
+
+original_list = list(
+    random.randint(0, 100)
+    for _ in range(10)
+)
+
+new_list = list()
+
+for num in range(len(original_list)):
+
+    if num % 2 == 1:
+        new_tuple = original_list[num - 1], original_list[num]
+        new_list.append(new_tuple)
+
+print(new_list)
