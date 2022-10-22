6f5accdb1a4da6ecf7064cf5ec13eb2327fb33c7
Андрей <icreem714@gmail.com>
2022-10-22 19:31:52 +0300

    Задача выполнена

--- Module20/09_competition_protocol/main.py
+++ Module20/09_competition_protocol/main.py
@@ -7,7 +7,7 @@ for num in range(1, record_count + 1):
     record = input('{0}-я запись: '.format(num)).split()
 
     if int(record[0]) > score_dict.get(record[1], 0):
-        score_dict[record[1]] = int(record[0]), count
+        score_dict[record[1]] = int(record[0])
 
 
 print('\nИтоги соревнований:')
@@ -15,8 +15,8 @@ print('\nИтоги соревнований:')
 for place in range(1, 4):
     max_score = max(score_dict.values())
 
-    for i_name, i_score in score_dict.items():
-        if max_score == i_score[0]:
+    for i_name, i_score in sorted(score_dict.items()):
+        if max_score == i_score:
             print('{0}-e место.'.format(place), i_name, i_score)
             break
 
