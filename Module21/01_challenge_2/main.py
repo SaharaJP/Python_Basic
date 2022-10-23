c7fa179fdfbc5c9831e6b4a8219bdc11f0199622
Андрей <icreem714@gmail.com>
2022-10-23 23:01:50 +0300

    Задача выполнена

--- Module21/01_challenge_2/main.py
+++ Module21/01_challenge_2/main.py
@@ -1,1 +1,12 @@
-# TODO здесь писать код
+def w_n_cycles(num):
+    if num == 0:
+        return 1
+
+    num = w_n_cycles(num - 1)
+    print(num)
+
+    return num + 1
+
+
+num = int(input('Введите num: '))
+w_n_cycles(num)
