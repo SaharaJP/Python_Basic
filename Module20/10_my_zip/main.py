52a464918fc60ef99239b35b53880336cc58adda
Андрей <icreem714@gmail.com>
2022-10-23 10:36:49 +0300

    Задача выполнена. Добавил вывод генератора

--- Module20/10_my_zip/main.py
+++ Module20/10_my_zip/main.py
@@ -1,13 +1,14 @@
 string = 'abcd'
 nums_tuple = (10, 20, 30, 40)
-result_list = list()
 
 print('Строка:', string)
 print('Кортеж чисел:', nums_tuple)
 
 print('\nРезультат:')
-for i in range(min(len(string), len(nums_tuple))):
-    result_list.append((string[i], nums_tuple[i]))
+result_list = ((string[i], nums_tuple[i])
+               for i in range(min(len(string), len(nums_tuple))))
+
+print(result_list)
 
 for part in result_list:
     print(part)
