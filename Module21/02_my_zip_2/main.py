56a59229d130e8b3122eb3096f44ad444ca7daa3
Андрей <icreem714@gmail.com>
2022-10-24 13:25:34 +0300

    Задача выполнена

--- Module21/02_my_zip_2/main.py
+++ Module21/02_my_zip_2/main.py
@@ -1,1 +1,17 @@
-# TODO здесь писать код
+def my_zip(a, b):
+
+    a, b= tuple(a), tuple(b)
+
+    result = [
+        (a[i], b[i])
+        for i in range(min(len(a), len(b)))
+    ]
+
+    return result
+
+
+a = [{"x": 4}, "b", "z", "d"]
+
+b = (10, {20,}, [30], "z")
+
+print(my_zip(a, b))
