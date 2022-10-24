bbb21f4ebd574c803922a6c58e6876363e136502
Андрей <icreem714@gmail.com>
2022-10-24 13:06:51 +0300

    Задача выполнена

--- Module21/03_fibonacci/main.py
+++ Module21/03_fibonacci/main.py
@@ -1,1 +1,9 @@
-# TODO здесь писать код
+def fibo(num):
+    if num in (1, 2):
+        return 1
+
+    return fibo(num - 1) + fibo(num - 2)
+
+
+num_pos = int(input('Введите позиция в ряде Фибоначчи: '))
+print('Число', fibo(num_pos))
