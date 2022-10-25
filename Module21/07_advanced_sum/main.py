2a265a78bcc42ed4a66cfbfdaa1af702089fdf38
Андрей <icreem714@gmail.com>
2022-10-25 12:56:07 +0300

    Задача выполнена

--- Module21/07_advanced_sum/main.py
+++ Module21/07_advanced_sum/main.py
@@ -1,1 +1,14 @@
-# TODO здесь писать код
+def summ(*args):
+    for i_el in args:
+        if isinstance(i_el, int):
+            return i_el
+
+        for j_el in i_el:
+            result = summ(j_el)
+            if result:
+                res.append(result)
+
+
+res = []
+summ([[1, 2, [3]], [1], 3])
+print(sum(res))
