7d6d5d3ffe0854a652647b50de87534133e85067
Андрей <icreem714@gmail.com>
2022-10-25 12:20:16 +0300

    Задача выполнена

--- Module21/06_deep_copy/main.py
+++ Module21/06_deep_copy/main.py
@@ -11,4 +11,34 @@ site = {
     }
 }
 
-# TODO здесь писать код
+def find_key(struct, key, value):
+    if key in struct:
+        struct[key] = value
+        return site
+
+    for el in struct.values():
+        if isinstance(el, dict):
+            result = find_key(el, key, value)
+            if result:
+                return site
+
+
+def new_sites(site, count):
+    if count == 0:
+        return site
+
+    name = input('Введите название для нового сайта: ')
+    keys = {'title': 'Куплю/продам {0} недорого'.format(name), 'h2': 'У нас самая низкая цена на {0}'.format(name)}
+
+    for key, value in keys.items():
+        find_key(site, key, value)
+
+    print('Сайт для {0}:'.format(name))
+    print(site, '\n')
+
+    new_sites(site, count - 1)
+    return site
+
+
+site_count = int(input('Сколько сайтов: '))
+new_sites(site, site_count)
