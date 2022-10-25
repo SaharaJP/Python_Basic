749e1eac7f0834f0306f4eb4195aed29e6d2f7c9
Андрей <icreem714@gmail.com>
2022-10-25 14:32:46 +0300

    Задача выполнена

--- Module21/08_list_of_lists_2/main.py
+++ Module21/08_list_of_lists_2/main.py
@@ -1,4 +1,18 @@
+def summ(*args):
+    for i_el in args:
+        if isinstance(i_el, int):
+            return i_el
+
+        for j_el in i_el:
+            result = summ(j_el)
+            if result:
+                res.append(result)
+
+
+
 nice_list = [1, 2, [3, 4], [[5, 6, 7], [8, 9, 10]],
              [[11, 12, 13], [14, 15], [16, 17, 18]]]
+res = []
+summ(nice_list)
 
-# TODO здесь писать код
+print('Ответ:', res)
