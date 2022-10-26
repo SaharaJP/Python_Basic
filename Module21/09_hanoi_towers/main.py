4ae2241952a9ef09b9c5732beef4bf66b9bea1d9
Андрей <icreem714@gmail.com>
2022-10-26 13:03:02 +0300

    Задача выполнена

--- Module21/09_hanoi_towers/main.py
+++ Module21/09_hanoi_towers/main.py
@@ -1,1 +1,12 @@
-# TODO здесь писать код
+def move(n, x, y, z):
+    if n == 0:
+        return
+
+    move(n - 1, x, z, y)
+    print('Переложить диск {0} со стержня {1} на стержень номер {2}'.format(n, x, z))
+    move(n - 1, y, x, z)
+
+    return
+
+num = int(input('Введите число дисков: '))
+move(num, 1, 2, 3)
