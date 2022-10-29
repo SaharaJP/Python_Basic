902c0af093c7d4ad4527d12bd4d6d3559e1ab13e
Андрей <icreem714@gmail.com>
2022-10-29 22:57:37 +0300

    Задача выполнена

--- Module21/05_make_function_faster/main.py
+++ Module21/05_make_function_faster/main.py
@@ -1,9 +1,19 @@
-def calculating_math_func(data):
-    result = 1
-    for index in range(1, data + 1):
-        result *= index
-    result /= data ** 3
-    result = result ** 10
-    return result
+def calculating_math_func(data, fact = {}):
+	if data in fact.keys():
+		result = fact[data]
+	else:
+		result = 1
+		for index in range(1, data + 1):
+			result *= index
+		fact[data] = result
 
-# TODO оптимизировать функцию
+	result /= data ** 3
+	result = result ** 10
+
+	return result
+
+
+calculating_math_func(10)
+calculating_math_func(10)
+calculating_math_func(11)
+
