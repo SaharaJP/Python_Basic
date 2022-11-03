dd691fc3a5ce628a0f055ac1d321ee9547bf8fae
Андрей <icreem714@gmail.com>
2022-11-03 22:42:13 +0300

    Задача выполнена

--- Module22/01_nums_sum_2/main.py
+++ Module22/01_nums_sum_2/main.py
@@ -1,1 +1,12 @@
-# TODO здесь писать код
+num = open('numbers.txt', 'r')
+ans = open('answer.txt', 'w')
+res = 0
+
+for line in num:
+    for el in line:
+        if el.isdigit():
+            res += int(el)
+ans.write(str(res))
+
+num.close()
+ans.close()
