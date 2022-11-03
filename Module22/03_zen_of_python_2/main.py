f732b4050c2ea8c591c7b26c7a683e75824c1081
Андрей <icreem714@gmail.com>
2022-11-04 00:09:03 +0300

    Задача выполнена

--- Module22/03_zen_of_python_2/main.py
+++ Module22/03_zen_of_python_2/main.py
@@ -1,5 +1,34 @@
 import os
 
 zen = os.path.abspath(os.path.join('..', '02_zen_of_python', 'zen.txt'))
-a = open(zen, 'r')
-print(a.read())
+zen_text = open(zen, 'r')
+
+sym_dict = dict()
+sym_count = 0
+word_count = 0
+line_count = 0
+
+for line in zen_text:
+    line_count += 1
+    word_count += len(line.split())
+
+    for sym in line:
+        if sym.isalpha():
+            sym_count += 1
+
+            if sym.lower() in sym_dict:
+                sym_dict[sym.lower()] += line.count(sym.lower())
+            else:
+                sym_dict[sym.lower()] = line.count(sym.lower())
+
+zen_text.close()
+
+for i_key in sym_dict.keys():
+    if sym_dict[i_key] == min(sym_dict.values()):
+        min_sym = i_key
+        break
+
+print('Количество букв в файле:', sym_count)
+print('Количество слов файле:', word_count)
+print('Количество строк в файле', line_count)
+print('Наиболее редкая буква', min_sym)
