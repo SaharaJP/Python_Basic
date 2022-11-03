44c5e4f9c8bb407e556bb3418550e979a0e2ddf1
Андрей <icreem714@gmail.com>
2022-11-03 23:30:48 +0300

    Задача выполнена

--- Module22/02_zen_of_python/main.py
+++ Module22/02_zen_of_python/main.py
@@ -1,1 +1,12 @@
-# TODO здесь писать код
+zen = open('zen.txt', 'r')
+
+count = zen.read().count('\n')
+
+for num in range(1, count + 2):
+    zen.seek(0)
+    line = zen.readlines()[-num]
+
+    if line.endswith('\n'):
+        line = line.replace('\n', '')
+
+    print(line)
