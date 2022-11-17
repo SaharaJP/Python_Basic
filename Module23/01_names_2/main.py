ed8b088b8fb318e673c6749c10bea95b9ed0a063
Андрей <icreem714@gmail.com>
2022-11-17 16:56:50 +0300

    Задача выполнена

--- Module23/01_names_2/main.py
+++ Module23/01_names_2/main.py
@@ -1,1 +1,15 @@
-# TODO здесь писать код
+len_sum = 0
+line_count = 0
+
+with open('people.txt', 'r', encoding = 'utf-8') as names:
+    for i_name in names:
+        line_count += 1
+        try:
+            if (len(i_name) - 1) > 3:
+                len_sum += len(i_name) - 1
+            else:
+                raise BaseException
+        except BaseException:
+            print('Ошибка: менее трёх символов в строке {0}'.format(line_count))
+
+    print('Общее количество символов:', len_sum)
