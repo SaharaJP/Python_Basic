e6afe295643195361ecd52fd5ae4faaee3fba18b
Андрей <icreem714@gmail.com>
2022-11-28 23:19:32 +0300

    Задача выполнена

--- Module23/03_lucky_number/main.py
+++ Module23/03_lucky_number/main.py
@@ -1,1 +1,21 @@
-# TODO здесь писать код
+import random
+
+total_num = 0
+
+with open('out_file.txt', 'w') as file:
+    try:
+        while True:
+            num = input('Введите число: ')
+            total_num += int(num)
+
+            if random.randint(1, 14) == 13:
+                raise BaseException
+
+            file.write(num + '\n')
+
+            if total_num >= 777:
+                print('Вы успешно выполнили условие для выхода из порочного цикла!')
+                break
+
+    except BaseException:
+        print('Вас поститгла неудача')
