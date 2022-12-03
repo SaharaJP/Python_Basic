06d9aae194d0fdc7678580b02ed05871f845e93b
Андрей <icreem714@gmail.com>
2022-12-04 00:33:53 +0300

    Задача выполнена

--- Module23/05_text_calc/main.py
+++ Module23/05_text_calc/main.py
@@ -1,1 +1,25 @@
-# TODO здесь писать код
+def syntax_error():
+    correct = input('Обнаружена ошибка в строке: {0}  Хотите исправить? '.format(line)).lower()
+
+    if correct == 'да':
+        new_line = input('Введите исправленную строку: ')
+        res_sum[0] += eval(new_line)
+
+
+res_sum = [0]
+
+with open('calc.txt', 'r') as calc:
+    for line in calc.read().split('\n'):
+
+        try:
+            for sym in {'+', '-', '*', '/', '//', '%'}:
+                if sym in line:
+                    res_sum[0] += eval(line)
+                    break
+            else:
+                raise SyntaxError
+
+        except SyntaxError:
+            syntax_error()
+
+    print('Сумма результатов:', res_sum)
