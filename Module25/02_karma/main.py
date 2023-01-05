d72cfbf6034e16bf42eb0bb3ffc0af8bc58075ef
andrewzhirov <andzero99@yandex.ru>
2023-01-06 01:43:57 +0300

    Задание выполнено.

--- Module25/02_karma/main.py
+++ Module25/02_karma/main.py
@@ -1,1 +1,47 @@
-# TODO здесь писать код
+import random
+
+karma_logs = open('karma.log.txt', 'w')
+
+class KillError(Exception):
+    def __init__(self):
+        karma_logs.write('Убил\n')
+
+class DrunkError(Exception):
+    def __init__(self):
+        karma_logs.write('Выпил\n')
+
+class CarCrashError(Exception):
+    def __init__(self):
+        karma_logs.write('Авария\n')
+
+class GluttonyError(Exception):
+    def __init__(self):
+        karma_logs.write('Обжорство\n')
+
+class DepressionError(Exception):
+    def __init__(self):
+        karma_logs.write('Депрессия\n')
+
+class Karma:
+    karma = 0
+
+    def one_day(self):
+        Karma.karma += random.randint(1, 7)
+
+        if random.randint(1, 10) == 1:
+            KillError()
+        elif random.randint(1, 10) == 2:
+            DrunkError()
+        elif random.randint(1, 10) == 3:
+            CarCrashError()
+        elif random.randint(1, 10) == 4:
+            GluttonyError()
+        elif random.randint(1, 10) == 5:
+            DepressionError()
+
+
+karma = Karma()
+while karma.karma < 500:
+    karma.one_day()
+
+karma_logs.close()
