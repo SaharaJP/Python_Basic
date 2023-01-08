75dafcd0038a509fb43997704a5d14b90db6fdee
andrewzhirov <andzero99@yandex.ru>
2023-01-09 00:25:48 +0300

    Задание выполнено.

--- Module25/03_my_dict/main.py
+++ Module25/03_my_dict/main.py
@@ -1,1 +1,13 @@
-# TODO здесь писать код
+class MyDict(dict):
+    def get(self, key):
+        if key in self.keys():
+            return self[key]
+        else:
+            return 0
+
+
+my_dict = MyDict()
+my_dict['a'] = 1
+
+print(my_dict.get('b'))
+print(my_dict.get('a'))
