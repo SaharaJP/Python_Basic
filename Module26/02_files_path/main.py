a66075096673eeb06508db84583ab75f572b1247
andrewzhirov <andzero99@yandex.ru>
2023-01-25 10:35:36 +0300

    Задание выполнено.

--- Module26/02_files_path/main.py
+++ Module26/02_files_path/main.py
@@ -1,1 +1,14 @@
-# TODO здесь писать код
+import os
+from collections.abc import Iterable
+
+
+def gen_files_path(cat: str, dir: str = '/') -> Iterable[str]:
+    for path, dir, file in os.walk(dir):
+        yield path
+
+
+directory = os.path.abspath(os.path.join('..', '..', '..', 'Python_Basic'))
+gen = gen_files_path('Module16', directory)
+
+for path in gen:
+    print(path)
