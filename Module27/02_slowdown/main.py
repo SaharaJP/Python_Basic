f3de01f6f0f1f326d708c290d7f1baf3a04d7640
andrewzhirov <andzero99@yandex.ru>
2023-01-30 22:33:45 +0300

    Задание выполнено.

--- Module27/02_slowdown/main.py
+++ Module27/02_slowdown/main.py
@@ -1,1 +1,23 @@
-# TODO здесь писать код
+from typing import Callable
+import functools
+import time
+
+
+def sleep_timer(func: Callable) -> Callable:
+    """Декоратор. Ожидающий перед выполнением функции несколько секунд"""
+
+    @functools.wraps(func)
+    def wraped() -> None:
+        time.sleep(3)
+        func()
+
+    return wraped
+
+@sleep_timer
+def test() -> None:
+    """Тестовая функция"""
+
+    print('Hello')
+
+
+test()
