5358060219966acd4727e5dfad3dddc091bb9dde
andrewzhirov <andzero99@yandex.ru>
2023-01-31 15:25:43 +0300

    Задание выполнено.

--- Module27/03_logging/main.py
+++ Module27/03_logging/main.py
@@ -1,1 +1,43 @@
-# TODO здесь писать код
+from typing import Callable
+import functools
+import datetime
+
+def logging(func: Callable) -> Callable:
+    """Декоратор. Отвечает за логирование функций"""
+
+    @functools.wraps(func)
+    def wr_log() -> None:
+        with open('function_errors.log', 'a') as errors:
+            try:
+                func()
+            except ZeroDivisionError:
+                print(
+                    '{name} - {document}\n'.format(
+                        name=func.__name__,
+                        document=func.__doc__
+                    )
+                )
+                errors.write(
+                    '{time}\t{name}\t{error}\n'.format(
+                        time=str(datetime.datetime.now()),
+                        name=func.__name__,
+                        error=str(ZeroDivisionError)
+                    )
+                )
+    return wr_log
+
+@logging
+def math_error():
+    """Функция, вызывающая ошибку деления на 0"""
+
+    raise ZeroDivisionError
+
+@logging
+def math_error_2():
+    """Функция, вызывающая ошибку деления на 0"""
+
+    raise ZeroDivisionError
+
+
+math_error()
+math_error_2()
