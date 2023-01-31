4520e02039d973e0ded086ff6eed9a0e4232c1ba
andrewzhirov <andzero99@yandex.ru>
2023-01-31 17:06:04 +0300

    Задание выполнено.

--- Module27/04_str_count/main.py
+++ Module27/04_str_count/main.py
@@ -1,1 +1,35 @@
-# TODO здесь писать код
+from typing import Callable
+import functools
+
+
+def counter(func: Callable) -> Callable:
+    """Декоратор. Считает и выводит, сколько раз вызвали функцию"""
+
+    @functools.wraps(func)
+    def wr_count() -> None:
+        wr_count.count += 1
+        func()
+        print(
+            'Функцию {name} вызвали {count} раз(а)'.format(
+                name=func.__name__,
+                count=wr_count.count
+            )
+        )
+
+    wr_count.count = 0
+    return wr_count
+
+
+@counter
+def first() -> None:
+    pass
+
+@counter
+def second() -> None:
+    pass
+
+first()
+second()
+
+first()
+first()
