946e77a19ef53b2d7f78d87139cc0df4efc33444
andrewzhirov <andzero99@yandex.ru>
2023-02-05 18:39:25 +0300

    Задание выполнено.

--- Module28/01_work_with_file_2/main.py
+++ Module28/01_work_with_file_2/main.py
@@ -1,1 +1,19 @@
-# TODO здесь писать код
+
+
+
+class File:
+    def __init__(self, file_name: str, type: str = 'w') -> None:
+        self.__file_name = file_name
+        self.__type = type
+
+    def __enter__(self):
+        self.file = open(self.__file_name, self.__type)
+        return self.file
+
+    def __exit__(self, exc_type, exc_val, exc_tb) -> bool:
+        self.file.close()
+        return True
+
+
+with File('test.txt', 'w') as test:
+    test.write('Привет!')
