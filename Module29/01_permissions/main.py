d4f555be50fe601fc0c4fe97ab18360c575ec751
andrewzhirov <andzero99@yandex.ru>
2023-02-12 13:26:44 +0300

    Задание выполнено.

--- Module29/01_permissions/main.py
+++ Module29/01_permissions/main.py
@@ -1,1 +1,42 @@
-# TODO здесь писать код
+from typing import Callable
+import functools
+
+
+class PermissionError(Exception):
+    def __str__(self):
+        return 'У пользователя недостаточно прав, чтобы выполнить функцию '
+
+
+def check_permission(name: str) -> Callable:
+    def permission_decorator(func: Callable) -> Callable:
+
+        @functools.wraps(func)
+        def wr_permission(*args, **kwargs) -> None:
+            try:
+
+                if name in user_permissions:
+                    func(*args, **kwargs)
+                else:
+                    raise PermissionError
+
+            except PermissionError as ex:
+                print(ex.__class__.__name__, end=': ')
+                print(ex, end=func.__name__)
+
+        return wr_permission
+    return permission_decorator
+
+
+user_permissions = ['admin']
+
+@check_permission('admin')
+def delete_site():
+    print('Удаляем сайт')
+
+@check_permission('user_1')
+def add_comment():
+    print('Добавляем комментарий')
+
+
+delete_site()
+add_comment()
