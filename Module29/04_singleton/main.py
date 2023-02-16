0d0c15b2ae5289c9313a04fc1d822cf92f9a434b
andrewzhirov <andzero99@yandex.ru>
2023-02-16 13:00:12 +0300

    Задание выполнено. Вынес переменную 'name_dict' за декоратор

--- Module29/04_singleton/main.py
+++ Module29/04_singleton/main.py
@@ -1,13 +1,14 @@
 import functools
 
 
+name_dict = dict()
+
 def singleton(cls):
     @functools.wraps(cls)
     def wr_singleton(*args, **kwargs):
         if cls.__name__ not in name_dict.keys():
             name_dict[cls.__name__] = cls(*args, *kwargs)
         return name_dict[cls.__name__]
-    name_dict = dict()
     return wr_singleton
 
 
