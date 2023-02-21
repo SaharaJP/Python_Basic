89fcfa628347c3f9d858e406a121994f33aad78b
andrewzhirov <andzero99@yandex.ru>
2023-02-21 23:53:21 +0300

    Задание выполнено.

--- Module30/03_palindrome_the_return/main.py
+++ Module30/03_palindrome_the_return/main.py
@@ -1,1 +1,14 @@
-# TODO здесь писать код
+import collections
+
+
+def can_be_poly(string: str) -> bool:
+    result = collections.deque(string)
+    while len(result) > 1:
+        if not result.pop() == result.popleft():
+            return False
+    else:
+        return True
+
+
+print(can_be_poly('abcba'))
+print(can_be_poly('abcbf'))
