c91489c623b67e9ae7b7f73a458ea8197e36cd9e
andrewzhirov <andzero99@yandex.ru>
2023-02-22 11:19:01 +0300

    Задание выполнено.

--- Module30/03_palindrome_the_return/main.py
+++ Module30/03_palindrome_the_return/main.py
@@ -1,14 +1,9 @@
-import collections
+from collections import Counter
 
 
 def can_be_poly(string: str) -> bool:
-    result = collections.deque(string)
-    while len(result) > 1:
-        if not result.pop() == result.popleft():
-            return False
-    else:
-        return True
+    return len(list(filter(lambda x: x % 2, Counter(string).values()))) <= 2
 
 
 print(can_be_poly('abcba'))
-print(can_be_poly('abcbf'))
+print(can_be_poly('abbbc'))
