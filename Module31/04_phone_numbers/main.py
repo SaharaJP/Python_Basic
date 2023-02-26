c3e9ff35af76dcf87813fffeae21716486f87907
andrewzhirov <andzero99@yandex.ru>
2023-02-26 12:17:54 +0300

    Задание выполнено.

--- Module31/04_phone_numbers/main.py
+++ Module31/04_phone_numbers/main.py
@@ -1,1 +1,10 @@
-# TODO здесь писать код
+import re
+
+num_list = ['9999999999', '999999-999', '99999x9999']
+num_pattern = r'[89]\d{9}'
+
+for num in num_list:
+    if re.match(num_pattern, num):
+        print(num_list.index(num) + 1, 'номер: все в порядке')
+    else:
+        print(num_list.index(num) + 1, 'номер: не подходит')
